24e0c0b463cf3ef61de557f70ea747e8842cc9bc
Jonah Kagan <jonah.kagan@clever.com>
2014-11-08 13:23:31 -0800

    initial animation of parametric equation for a circle

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2202"]
+                 [org.clojure/clojurescript "0.0-2311"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [om "0.5.3"]]
+                 [om "0.7.3"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
